289dad6d3f1fd4bc5f2ad7fd676472c1a8fe34a5
jaz016 <xjet16@gmail.com>
2016-05-01 23:59:30 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,122 @@
-var clickNumber = 0;
-var timebarValue = 100;
 var timeLeft = 10000;
+var timebarValue = 100;
+var clickNumber = 0;
 var sec = timeLeft/1000;
 var resetTimebar = timebarValue;
 var resetTimeLeft = timeLeft;
 var resetSec = resetTimeLeft/1000;
 var resetClicks = clickNumber;
 
-function popup() {
-	var timebarValue = resetTimebar;
-	var timeLeft = resetTimeLeft;
-	var sec = resetSec;
-	if(clickNumber==1) {
-		alert("Time's up! You've made "+ clickNumber + " click!");
-	} else {
-		alert("Time's up! You've made "+ clickNumber + " clicks!");
+var start = document.getElementById("start");
+var clicksLabel = document.getElementById("clicks");
+var timerArea = document.getElementById("timer-area");
+
+function checkPlural(x) {
+
+	if(x===1) {
+		return true;
 	}
 
+}
+
+function resetGame() {
+
+	start.className = "";
+	document.getElementById("btn-message").innerHTML = "Start";
+	toggleDetails();
+
+}
+
+function showMessage() {
+
+	if(checkPlural(clickNumber)) {
+		alert("Time's up! You've made "+ clickNumber + " click!")
+	} else {
+		alert("Time's up! You've made "+ clickNumber + " clicks!")
+	}
+
+	timebarValue = resetTimebar;
+	timeLeft = resetTimeLeft;
+	sec = resetSec;
 	clickNumber = resetClicks;
 
-	clearInterval(countdownHandle);
-	document.getElementById("timebar").style.width = timebarValue + "%";
-	document.getElementById("timer").innerHTML = sec + " seconds left";
-	if(clickNumber==1) {
-		document.getElementById("clicks").innerHTML = clickNumber + " click!"
-	} else {
-		document.getElementById("clicks").innerHTML = clickNumber + " clicks!"
-	}
+	clearInterval(intervalHandle);
 
-	document.getElementById("trigger").innerHTML = "<div id=\"start\"></div>Start";
+	resetGame();
 
 }
 
 function countClicks() {
+
 	clickNumber++;
-	if(clickNumber==1) {
-		document.getElementById("clicks").innerHTML = clickNumber + " click!"
+
+	if(checkPlural(clickNumber)) {
+		clicksLabel.innerHTML = clickNumber + " click!"
 	} else {
-		document.getElementById("clicks").innerHTML = clickNumber + " clicks!"
+		clicksLabel.innerHTML = clickNumber + " clicks!"
 	}
-}
 
-function showDetails() {
-
-	document.getElementById("clicks").style.display = "block";
-	document.getElementById("timer-area").style.display = "block";
-}
-
-function countdown() {
-	document.getElementById("timebar").style.width = timebarValue + "%";
-	timebarValue=timebarValue-(timeLeft/(timeLeft/100))/(timeLeft/1000);
-	document.getElementById("timer").innerHTML = sec + " seconds left";
-	sec--;
 }
 
 document.getElementById("trigger").onclick = function() {
 	countClicks();
 };
 
-document.getElementById("start").onclick = function() {
-	countdown();
-	countdownHandle = setInterval(countdown, 1000);
-	showDetails();
-	setTimeout(popup, timeLeft);
-	document.getElementById("trigger").innerHTML = "Click me fast!";
+function toggleDetails() {
+
+	if((clicksLabel.className==="hide") && (timerArea.className==="hide")) {
+		clicksLabel.className="";
+		timerArea.className="";
+	} else {
+		clicksLabel.className="hide";
+		timerArea.className="hide";
+	}
+
+}
+
+function decreaseTime() {
+
+	document.getElementById("timebar").style.width = timebarValue + "%";
+	timebarValue-=(timeLeft/(timeLeft/100))/(timeLeft/1000);
+
+	if(sec===1) {
+		document.getElementById("timer").innerHTML = sec + " second left";
+	} else {
+		document.getElementById("timer").innerHTML = sec + " seconds left";
+	}
+
+	sec--;
+
+}
+
+start.onclick = function() {
+
+	decreaseTime();
+	intervalHandle = setInterval(decreaseTime, 1000);
+	toggleDetails();
+	setTimeout(showMessage, timeLeft);
+	start.className = "hide";
+	document.getElementById("btn-message").innerHTML = "Click me fast!";
+
 };
+
+window.onload = function() {
+
+	var btnMessage = document.createElement("p");
+	var message = document.createTextNode("Start");
+	btnMessage.setAttribute("id", "btn-message");
+	btnMessage.appendChild(message);
+	document.getElementById("trigger").appendChild(btnMessage);
+
+};
+
+
+//Changing color effect
+
+document.getElementById("trigger").onmousedown = function() {
+	this.setAttribute("style", "background:#0E85BB;");
+}
+
+document.getElementById("trigger").onmouseup = function() {
+	this.setAttribute("style", "background:#03A9F4;");
+}
